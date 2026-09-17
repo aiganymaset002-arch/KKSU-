80629f20ff2927bf843d4118bdc9653c237a7c04
aiganymaset002-arch <aiganymaset002@gmail.com>
2026-09-16 19:49:31 -0700

    Add files via upload

---
+++ TeacherHomeView.swift
@@ -0,0 +1,7 @@
+//
+//  TeacherHomeView.swift
+//  Comfort School-Univercity
+//
+//  Created by MacBook on 21.08.2026.
+//
+
---
+++ TeacherStudentsView.swift
@@ -0,0 +1,7 @@
+//
+//  TeacherStudentsView.swift
+//  Comfort School-Univercity
+//
+//  Created by MacBook on 21.08.2026.
+//
+
